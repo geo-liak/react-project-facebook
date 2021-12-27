13117d4f3c0a05fff8851d2f4b0e3597896382d8
geo-liak <liakopoulos.georgios@gmail.com>
2021-12-28 01:16:50 +0200

    json server and login logic

--- src/components/myButton.js
+++
@@ -1,9 +0,0 @@
-import Button  from 'react-bootstrap/Button';
-import { propTypes } from 'react-bootstrap/esm/Image';
-
-export default function MyButton(props) {
-
-    return (
-        <Button className="btn success" style={{ border: "red"}}>{props.btnText}</Button>
-    )
-}
